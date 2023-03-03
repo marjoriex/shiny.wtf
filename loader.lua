6fb5d5eb679d6d60adeb03a5a2b3ca8ff03421b3
marjorie <111649405+marjoriex@users.noreply.github.com>
2023-03-03 18:55:27 +0000

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,5 @@
+function get(file)
+    return loadstring(game:HttpGetAsync(("https://raw.githubusercontent.com/marjoriex/shiny.wtf/main/%.lua"):format(file)), file .. '.lua')()
+end
+
+get("main");
