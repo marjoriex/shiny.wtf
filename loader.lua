cdffeb1151420d88589bd28c574d595d25c9c825
marjorie <111649405+marjoriex@users.noreply.github.com>
2023-03-04 11:35:42 +0000

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,4 +1,4 @@
-function get(file)
+local function get(file)
     return loadstring(game:HttpGetAsync(("https://raw.githubusercontent.com/marjoriex/shiny.wtf/main/%.lua"):format(file)), file .. '.lua')()
 end
 
